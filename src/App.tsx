e9bdc7b1d652ee3c8fab3df4e2abb375a021c4b6
Lucas Livchits <livchitsl@gmail.com>
2021-11-03 23:25:48 -0300

    Adds logic to calculate points

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,22 @@ import './App.css';
 
 import getPeon from './utils/getPeon';
 import getCellBackground from './utils/getCellBackground';
+import getPeonsQuantityByColors from './utils/getPeonsQuantityByColor';
+import { Cell } from './types';
 
 const ROWS = 8;
 const COLUMNS = 10;
 
-const cells = [...Array(ROWS * COLUMNS)].map((_, index) => ({
+const cells: Cell[] = [...Array(ROWS * COLUMNS)].map((_, index) => ({
   cellNumber: index,
   peon: getPeon(index, COLUMNS, ROWS),
   backgroundColor: getCellBackground(index, COLUMNS),
 }));
 
 function App() {
-  const whitePoints =
-    (COLUMNS * 3) / 2 - cells.filter(({ peon }) => peon === 'B').length;
+  const peonsPerColor = (COLUMNS * 3) / 2;
+  const whitePoints = peonsPerColor - getPeonsQuantityByColors(cells, 'B');
+  const blackPoints = peonsPerColor - getPeonsQuantityByColors(cells, 'W');
 
   return (
     <main>
