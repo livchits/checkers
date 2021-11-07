43fe98aaffa21c7e7e7abc808f1bd77ff0954c6b
Lucas Livchits <livchitsl@gmail.com>
2021-11-07 00:29:52 -0300

    Uses optional chaining to prevent look for peons outside the board

--- src/utils/getValidMoves.ts
+++ src/utils/getValidMoves.ts
@@ -10,18 +10,16 @@ export default function getValidMoves(cellNumber: number, board: Cell[]) {
     selectedPeonColor === 'white'
       ? [cellNumber + 9, cellNumber + 11]
       : [cellNumber - 9, cellNumber - 11];
-  console.log({ adjacentMoves });
+
   const possibleMoves = adjacentMoves
-    .filter(moveIsInBoard)
-    .filter((move) => board[move].peon !== selectedPeonColor)
+    .filter((move) => board[move]?.peon !== selectedPeonColor)
     .map((move) =>
       //check if the move is an enemy peon or an empty cell
       //if it's an enemy return the cell after take it
-      !board[move].peon ? move : move - cellNumber + move
+      !board[move]?.peon ? move : move - cellNumber + move
     )
-    .filter(moveIsInBoard)
     //remove moves with peons
-    .filter((move) => board[move].peon === null)
+    .filter((move) => board[move]?.peon === null)
     //remove cells where couldn't be a peon
     .filter((move) => board[move].backgroundColor === 'bg-dark');
 
